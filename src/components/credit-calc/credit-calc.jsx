482559e187fb7bc284dd1a299168990dafcabef3
Ivan Lysukhin <ilysukhin@mail.ru>
2021-10-03 17:27:45 +0300

    select

--- src/components/credit-calc/credit-calc.jsx
+++ src/components/credit-calc/credit-calc.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Select from 'react-select';
 
 const options = [
@@ -13,30 +13,69 @@ const customStyles = {
     border: '1px solid #1F1E25',
     borderRadius: '0 0 4px 4px',
     borderTop: 'none',
+    position: 'absolute',
+    left: '0',
+    right: '0',
+    zIndex: '100',
+    backgroundColor: 'white',
   }),
-  placeholder: (base, state) => ({...base, color: 'tomato'}),
-  control: (base, state) => {
-    console.log(state);
-    return {
+  valueContainer:(base, state) => (
+    {
       ...base,
-      color: 'tomato',
-      display: 'flex',
-      flexDirection: 'row',
-    };},
+      color: '#1F1E25',
+      padding: '17px 15px 18px 15px',
+      fontSize: '16px',
+      fontWeight: '500',
+    }
+  ),
+  placeholder: (base, state) => (
+    {
+      ...base,
+      color: '#1F1E25',
+      fontSize: '16px',
+      fontWeight: '500',
+      outline: state.isFocused ? '1px solid rad' : 'none',
+    }
+  ),
+  option: (base, state) => ({
+    borderBottom: state.value === 'mortgage' ? '1px solid #C1C2CA' : 'none',
+    padding: '17px 15px 18px 23px',
+    fontSize: '16px',
+    margin: '0',
+    cursor: 'pointer',
+    color: state.isFocused ? '#2C36F2' : '',
+  }),
+  control: (base, state) => ({
+    border: '1px solid #1F1E25',
+    borderRadius: state.menuIsOpen ? '4px 4px 0 0' : '4px',
+    boxShadow: 'none',
+    color: 'tomato',
+    display: 'flex',
+    flexDirection: 'row',
+    borderColor: state.isFocused ? '#2C36F2' : '#1F1E25',
+  }),
 };
 
 function CreditCalc() {
+  const [menuState, setMenuState] = useState(false);
   return (
     <section className="credit-calc">
       <h3 className="credit-calc__main-title">Кредитный калькулятор</h3>
       <div className="credit-calc__step-one">
         <h4 className="credit-calc__step-title">Шаг 1. Цель кредита</h4>
         <label className="visually-hidden" htmlFor="creditType">цель кредита</label>
-        <Select
-          styles={customStyles}
-          placeholder="Выберите цель кредита"
-          options={options}
-        />
+        <div className={`credit-calc__select-container ${menuState && 'credit-calc__select-container--open'}`}>
+          <Select
+            styles={customStyles}
+            placeholder="Выберите цель кредита"
+            options={options}
+            isSearchable={false}
+            className="test"
+            onMenuOpen={() => {setMenuState(true);}}
+            onMenuClose={() => {setMenuState(false);}}
+            openMenuOnFocus
+          />
+        </div>
       </div>
       <div className="credit-calc__step-two">
         <h4 className="credit-calc__step-title">Шаг 2. Введите параметры кредита</h4>
