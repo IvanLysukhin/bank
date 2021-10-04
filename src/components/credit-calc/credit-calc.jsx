2fd3d28e3d82c0549ee415024951e0e1370a578a
Ivan Lysukhin <ilysukhin@mail.ru>
2021-10-04 16:55:14 +0300

    style for first two steps calc

--- src/components/credit-calc/credit-calc.jsx
+++ src/components/credit-calc/credit-calc.jsx
@@ -1,5 +1,8 @@
 import React, {useState} from 'react';
+import Slider, { SliderTooltip } from 'rc-slider';
+import 'rc-slider/assets/index.css';
 import Select from 'react-select';
+import NumberFormat from 'react-number-format';
 
 const options = [
   { value: 'mortgage', label: 'Ипотечное кредитование' },
@@ -55,6 +58,27 @@ const customStyles = {
     borderColor: state.isFocused ? '#2C36F2' : '#1F1E25',
   }),
 };
+const { Handle } = Slider;
+
+const handle = (props) => {
+  const { value, dragging, index, min, ...restProps } = props;
+  let overlay = `${value} %`;
+  if (min === 5) {
+    overlay = `${value} лет`;
+  }
+
+  return (
+    <SliderTooltip
+      prefixCls="rc-slider-tooltip"
+      overlay={overlay}
+      visible
+      placement="bottom"
+      key={index}
+    >
+      <Handle value={value} {...restProps} />
+    </SliderTooltip>
+  );
+};
 
 function CreditCalc() {
   const [menuState, setMenuState] = useState(false);
@@ -79,9 +103,116 @@ function CreditCalc() {
       </div>
       <div className="credit-calc__step-two">
         <h4 className="credit-calc__step-title">Шаг 2. Введите параметры кредита</h4>
+        <div className="credit-calc__step-container">
+          <label className="credit-calc__label" htmlFor="price">Стоимость недвижимости</label>
+          <div className="credit-calc__input-container">
+            <button className="credit-calc__btn credit-calc__btn--decrease" area-label="Уменьшить">
+              <svg width="16" height="2" viewBox="0 0 16 2" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <line y1="1" x2="16" y2="1" stroke="#1F1E25" strokeWidth="2"/>
+              </svg>
+            </button>
+            <NumberFormat
+              className="credit-calc__input"
+              thousandSeparator={' '}
+              suffix={' рублей'}
+              id="price"
+              name="price"
+            />
+            <button className="credit-calc__btn credit-calc__btn--increase" area-label="Увеличить">
+              <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M0 8H16M8 0V16" stroke="#1F1E25" strokeWidth="2"/>
+              </svg>
+            </button>
+          </div>
+          <p className="credit-calc__warning-info">От 1 200 000  до 25 000 000 рублей</p>
+        </div>
+        <div className="credit-calc__step-container">
+          <label className="credit-calc__label" htmlFor="initialFee">Первоначальный взнос</label>
+          <div className="credit-calc__input-container">
+            <NumberFormat
+              className="credit-calc__input"
+              thousandSeparator={' '}
+              suffix={' рублей'}
+              id="initialFee"
+              name="initialFee"
+            />
+          </div>
+          <div className="credit-calc__scrollbar-container">
+            <Slider
+              min={10}
+              max={100}
+              handle={handle}
+              step={5}
+              railStyle={{ backgroundColor: '#C1C2CA', height: 1 }}
+              trackStyle={{ backgroundColor: '#2C36F2', height: 1 }}
+              handleStyle={{
+                border: 'none',
+                height: 14,
+                width: 14,
+                backgroundColor: '#2C36F2',
+                boxShadow: 'none',
+              }}
+            />
+          </div>
+        </div>
+        <div className="credit-calc__step-container credit-calc__step-container--time">
+          <label className="credit-calc__label" htmlFor="loanTerms">Срок кредитования</label>
+          <div className="credit-calc__input-container">
+            <NumberFormat
+              className="credit-calc__input"
+              thousandSeparator={' '}
+              suffix={' лет'}
+              id="loanTerms"
+              name="loanTerms"
+            />
+          </div>
+          <div className="credit-calc__scrollbar-container credit-calc__scrollbar-container--time">
+            <Slider
+              min={5}
+              max={30}
+              handle={handle}
+              step={1}
+              railStyle={{ backgroundColor: '#C1C2CA', height: 1 }}
+              trackStyle={{ backgroundColor: '#2C36F2', height: 1 }}
+              handleStyle={{
+                border: 'none',
+                height: 14,
+                width: 14,
+                backgroundColor: '#2C36F2',
+                boxShadow: 'none',
+              }}
+              mode={'time'}
+            />
+          </div>
+        </div>
+        <div className="credit-calc__step-container">
+          <input className="visually-hidden credit-calc__checkbox-input" type="checkbox" id="mother" name="mother"/>
+          <label className="credit-calc__checkbox-label" htmlFor="mother">Использовать материнский капитал</label>
+        </div>
       </div>
       <div className="credit-calc__offer">
-        <h4 className="credit-calc__step-title">Наше предложение</h4>
+        <div className="credit-calc__offer-container">
+          <h4 className="credit-calc__step-title credit-calc__step-title--offer">Наше предложение</h4>
+          <ul className="credit-calc__offer-list">
+            <li className="credit-calc__offer-item">
+              <p className="credit-calc__offer-value">1 330 000 рублей </p>
+              <p className="credit-calc__offer-label">Сумма ипотеки</p>
+            </li>
+            <li className="credit-calc__offer-item">
+              <p className="credit-calc__offer-value">9,40%</p>
+              <p className="credit-calc__offer-label">Процентная ставка</p>
+            </li>
+            <li className="credit-calc__offer-item">
+              <p className="credit-calc__offer-value">27 868 рублей</p>
+              <p className="credit-calc__offer-label">Ежемесячный платеж</p>
+            </li>
+            <li className="credit-calc__offer-item">
+              <p className="credit-calc__offer-value">61 929 рублей</p>
+              <p className="credit-calc__offer-label">Необходимый доход</p>
+            </li>
+          </ul>
+          <button className="credit-calc__offer-btn">Оформить заявку</button>
+        </div>
       </div>
       <div className="credit-calc__application">
         <h4 className="credit-calc__step-title">Шаг 3. Оформление заявки</h4>
